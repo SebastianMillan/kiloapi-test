4940e0050c8bbddc730a49896ac942e647f0fca3
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-01-19 13:09:36 +0100

    Crear tests

---
+++ src/test/java/com/salesianostriana/kilo/repositories/ClaseRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.salesianostriana.kilo.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClaseRepositoryTest {
+
+    @Test
+    void findKilos() {
+    }
+
+    @Test
+    void findClasesOrderedByRank() {
+    }
+}
